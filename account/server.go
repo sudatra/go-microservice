8a4da95581674b8492ebeb4da81155f76f626366
sudatra <mail2sudatra@gmail.com>
2024-12-01 19:00:10 +0530

    added grpc server template code

--- account/server.go
+++ account/server.go
@@ -1,1 +1,67 @@
 package account
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	server Service
+}
+
+func ListenGRPC(s Service, port int) error {
+  lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port));
+  if err != nil {
+    return err
+  }
+
+  serv := grpc.NewServer();
+  reflection.Register(serv);
+
+  return serv.Serve(lis) 
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
+  a, err := s.server.PostAccount(ctx, r.Name);
+  if err != nil {
+    return nil, err;
+  }
+
+  return &pb.PostAccountResponse{Account: &pb.Account{
+    Id: a.ID,
+    Name: a.Name,
+  }}, nil
+}
+
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+  a, err := s.server.GetAccount(ctx, r.Id);
+  if err != nil {
+    return nil, err;
+  }
+
+  return &pb.GetAccountResponse{Account: &pb.Account{
+    Id: a.ID,
+    Name: a.Name,
+  }}, nil
+}
+
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+  res, err := s.server.GetAccounts(ctx, r.skip, r.take);
+  if err != nil {
+    return nil, err;
+  }
+
+  accounts := []*pb.Account{};
+  for _, a := range res {
+    accounts = append(accounts, &pb.Account{
+      Id: a.ID,
+      Name: a.Name,
+    })
+  }
+
+  return &pb.GetAccountsResponse{Accounts: accounts}, nil
+}
--- go.mod
+++ go.mod
@@ -17,4 +17,10 @@ require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/grpc v1.68.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 )
