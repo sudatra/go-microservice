c2178603d93e4109f6959eda5cb9eb46ed935738
Sudatra Ghosh <sudatraghosh@jmangroup.com>
2024-12-02 12:24:22 +0530

    Finished accounts microservice, added grpc client server connection

---
+++ account/cmd/account/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sudatra/go-microservice/account"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+
+	err := envconfig.Process("", &cfg);
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	var r account.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = account.NewPostgresRepository(cfg.DatabaseURL);
+		if err != nil {
+			log.Println(err);
+		}
+
+		return
+	})
+
+	defer r.Close();
+	log.Println("Listening on port 8080");
+
+	s := account.NewAccountService(r);
+	log.Fatal(account.ListenGRPC(s, 8080))
+}
